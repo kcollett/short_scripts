158750cc3bb2b5fcbb0dd6dcf6b7b493491603cd
kcollett <karen.collett@gmail.com>
2022-09-20 11:22:27 -0500

    Add support for reading from files.

--- get_rates.py
+++ get_rates.py
@@ -1,9 +1,10 @@
-#! /usr/bin/python3
+#! /usr/local/bin/python3
 #
 #  -*- mode: python; -*-
 #
 import datetime
 import xml.etree.cElementTree as ET
+from abc import ABC, abstractclassmethod
 from dataclasses import dataclass
 
 import requests
@@ -20,13 +21,43 @@ class Rates:
     r30y: float
 
 
-TREASURE_RATE_XML_URL = "https://home.treasury.gov/resource-center/data-chart-center/interest-rates/pages/xml"
+class RootBuilter(ABC):
+    @abstractclassmethod
+    def getRoot() -> ET.Element:
+        pass
+
+
+class UrlRootBuilder(RootBuilter):
+    url: str
+
+    def __init__(self, url: str):
+        self.url = url
+
+    def getRoot(self) -> ET.Element:
+        page = requests.get(self.url)
+        root = ET.fromstring(page.content)
+        return root
+
+
+class FileRootBuilder(RootBuilter):
+    file: str
+
+    def __init__(self, file: str):
+        self.file = file
+
+    def getRoot(self) -> ET.Element:
+        tree = ET.parse(self.file)
+        root = tree.getroot()
+        return root
+
+
+TREASURY_RATE_XML_URL = "https://home.treasury.gov/resource-center/data-chart-center/interest-rates/pages/xml"
 DATA_NAME = "data"
 NOMINAL_DATA_VALUE = "daily_treasury_yield_curve"
 REAL_DATA_VALUE = "daily_treasury_real_yield_curve"
 DATE_VALUE_MONTH_NAME = "field_tdr_date_value_month"
-BASE_NOMINAL_XML_URL = f"{TREASURE_RATE_XML_URL}?{DATA_NAME}={NOMINAL_DATA_VALUE}"
-BASE_REAL_XML_URL = f"{TREASURE_RATE_XML_URL}?{DATA_NAME}={REAL_DATA_VALUE}"
+BASE_NOMINAL_XML_URL = f"{TREASURY_RATE_XML_URL}?{DATA_NAME}={NOMINAL_DATA_VALUE}"
+BASE_REAL_XML_URL = f"{TREASURY_RATE_XML_URL}?{DATA_NAME}={REAL_DATA_VALUE}"
 
 
 def find_all_ending_with(element: ET.Element, tag: str) -> list[ET.Element]:
@@ -51,8 +82,9 @@ def get_last_entry(root: ET.Element) -> ET.Element:
 
 def extract_date_from_element(date_elt: ET.Element) -> datetime.date:
     date_str = date_elt.text.strip()
-    dt = datetime.datetime.strptime(date_str,"%Y-%m-%dT%H:%M:%S")
+    dt = datetime.datetime.strptime(date_str, "%Y-%m-%dT%H:%M:%S")
     return dt.date()
+
 
 def extract_rate_from_element(rate_elt: ET.Element) -> float:
     return float(rate_elt.text.strip()) / 100
@@ -80,9 +112,8 @@ def extract_rates_from_properties(properties: ET.Element) -> Rates:
     return Rates(date, r5y, r10y, r20y, r30y)
 
 
-def get_rates(url: str) -> Rates:
-    page = requests.get(url)
-    root = ET.fromstring(page.content)
+def get_rates(rb: RootBuilter) -> Rates:
+    root = rb.getRoot()
 
     last_elt = get_last_entry(root)
     content = find_only_one_ending_with(last_elt, "content")
@@ -98,8 +129,13 @@ def main() -> None:
     nominal_url = f"{BASE_NOMINAL_XML_URL}&{DATE_VALUE_MONTH_NAME}={date_value_month}"
     real_url = f"{BASE_REAL_XML_URL}&{DATE_VALUE_MONTH_NAME}={date_value_month}"
 
-    nominal_rates = get_rates(nominal_url)
-    real_rates = get_rates(real_url)
+    # nominal_rates = get_rates(UrlRootBuilder(nominal_url))
+    # real_rates = get_rates(UrlRootBuilder(real_url))
+
+    nominal_rates = get_rates(
+        FileRootBuilder("/Users/karencollett/treasury_nominal.xml")
+    )
+    real_rates = get_rates(FileRootBuilder("/Users/karencollett/treasury_real.xml"))
 
     print(nominal_rates)
     print(real_rates)
